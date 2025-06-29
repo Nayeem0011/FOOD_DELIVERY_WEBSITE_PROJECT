103feda0b1d25206052e9ccef23c089b1f45b835
Sk Nayeem <sknayeemislam384@gmail.com>
2025-06-29 16:54:12 +0600

    Navber Add

---
+++ src/Category.jsx
@@ -0,0 +1,40 @@
+import { MdOutlineDensitySmall } from "react-icons/md";
+
+export const categories=[
+    {
+        id:1,
+        name:"All",
+        image:<MdOutlineDensitySmall />
+    },
+    {
+        id:2,
+        name:"All",
+        image:<MdOutlineDensitySmall />
+    },
+    {
+        id:3,
+        name:"All",
+        image:<MdOutlineDensitySmall />
+    },
+    {
+        id:4,
+        name:"All",
+        image:<MdOutlineDensitySmall />
+    },
+    {
+        id:5,
+        name:"All",
+        image:<MdOutlineDensitySmall />
+    },
+    {
+        id:6,
+        name:"All",
+        image:<MdOutlineDensitySmall />
+    },
+    {
+        id:7,
+        name:"All",
+        image:<MdOutlineDensitySmall />
+    },
+
+]
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react'
+import Navber from '../components/Navber'
+
+const Homepage = () => {
+  return (
+    <Fragment>
+        <div className='bg-slate-200 w-full min-h-screen'>
+          <Navber/>
+        </div>
+    </Fragment>
+  )
+}
+
+export default Homepage
